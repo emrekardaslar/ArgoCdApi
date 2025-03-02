c599c76f181f89d39f21419d97242fbd5482f5dc
emrekardaslar <emre.kardaslar@gmail.com>
2025-03-02 17:37:01 +0300

    use hardcoded port

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => Results.Ok(new { message = "Hello from .NET 8 API in Docker!" }));
 
-// Read port from environment variable (default to 5041)
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5041";
-app.Urls.Add($"http://0.0.0.0:{port}");
+app.Urls.Add("http://0.0.0.0:5041");
 
 app.Run();
